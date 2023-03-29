9c5d1d81fbdd65a63c3ec3ae34648295492b1b53
Keith Noguchi <keith@noguchi.us>
2023-03-28 18:13:31 -0700

    Print out the http.Request contents

--- ch01/main.go
+++ ch01/main.go
@@ -1,13 +1,4 @@
 // A web server
-//
-// # Examples
-// ```
-// $ go run main.go
-// 2023/03/28 18:03:29 counter
-// 2023/03/28 18:03:29 handler
-// 2023/03/28 18:03:30 counter
-// 2023/03/28 18:03:30 handler
-// ```
 package main
 
 import (
@@ -27,16 +18,25 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Println("handler")
 	lock.Lock()
 	count++
 	lock.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
 
 func counter(w http.ResponseWriter, r *http.Request) {
-	log.Println("counter")
 	lock.Lock()
-	fmt.Fprintf(w, "Count %d\n", count)
+	fmt.Fprintf(w, "Count = %d\n", count)
 	lock.Unlock()
 }
