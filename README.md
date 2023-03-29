dfd8cc298f33c8ccf5b4becfec7d5d4605eefccb
Keith Noguchi <keith@noguchi.us>
2023-03-28 17:36:41 -0700

    A lissajous generator with io.Writer

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Examples from [the Go Programming Language], aka, the book.
 
 ## Examples
 
-- [Chapter 1: A duplicate line detector](ch01/main.go)
+- [Chapter 1: A lissajous generator](ch01/main.go)
 
 Happy Hacking!
--- ch01/main.go
+++ ch01/main.go
@@ -1,29 +1,53 @@
-// A duplicate line detector.
+// A lissajous generator
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
 	"os"
-	"strings"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
 )
 
 func main() {
-	counter := make(map[string]int)
+	lissajous(os.Stdout)
+}
 
-	for _, file := range os.Args[1:] {
-		data, err := ioutil.ReadFile(file)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "ch01: %v\n", err)
-			continue
+func lissajous(w io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(
+				size+int(x*size+0.5),
+				size+int(y*size+0.5),
+				blackIndex,
+			)
 		}
-		for _, line := range strings.Split(string(data), "\n") {
-			counter[line]++
-		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
 	}
-	for line, n := range counter {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
+	gif.EncodeAll(w, &anim)
 }
