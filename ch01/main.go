d5823cb0a0c85935746b600e8707f44ee9e457eb
Keith Noguchi <keith@noguchi.us>
2023-03-28 18:26:10 -0700

    a lissajous web server

--- ch01/main.go
+++ ch01/main.go
@@ -1,42 +1,60 @@
-// A web server
+// A lissajous web server
+//
+// # Examples
+//
+// ```
+// $ go run main.go
+// ```
 package main
 
 import (
-	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
-	"sync"
 )
 
-var lock sync.Mutex
-var count int
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	lock.Lock()
-	count++
-	lock.Unlock()
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(
+				size+int(x*size+0.5),
+				size+int(y*size+0.5),
+				blackIndex,
+			)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
 	}
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
-	}
-	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
-	}
-}
-
-func counter(w http.ResponseWriter, r *http.Request) {
-	lock.Lock()
-	fmt.Fprintf(w, "Count = %d\n", count)
-	lock.Unlock()
+	gif.EncodeAll(w, &anim)
 }
