200a316757566ea0ce566777e84461c036459cc3
Keith Noguchi <keith@noguchi.us>
2023-03-29 21:51:42 -0700

    go fmt

--- ch02/main.go
+++ ch02/main.go
@@ -5,8 +5,8 @@ import "fmt"
 
 func main() {
 	var freezingF, boilingF = 32.0, 212.0
-	fmt.Printf("%gF = %gC\n", freezingF, fToC(freezingF));
-	fmt.Printf("%gF = %gC\n", boilingF, fToC(boilingF));
+	fmt.Printf("%gF = %gC\n", freezingF, fToC(freezingF))
+	fmt.Printf("%gF = %gC\n", boilingF, fToC(boilingF))
 }
 
 func fToC(f float64) float64 {
