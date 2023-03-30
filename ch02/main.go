e68c0ba9a7ccaacfebebf5a615250d41c6ae908d
Keith Noguchi <keith@noguchi.us>
2023-03-29 21:41:35 -0700

    go fmt

--- ch02/main.go
+++ ch02/main.go
@@ -8,5 +8,5 @@ const boilingF = 212.0
 func main() {
 	var f = boilingF
 	var c = (f - 32) * 5 / 9
-	fmt.Printf("boiling point = %gF or %gC\n", f, c);
+	fmt.Printf("boiling point = %gF or %gC\n", f, c)
 }
