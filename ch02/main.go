126a56ffd5ca2f6f2856a7400256146ad1a744bc
Keith Noguchi <keith@noguchi.us>
2023-03-29 21:51:07 -0700

    function, func, declaration example

--- ch02/main.go
+++ ch02/main.go
@@ -1,12 +1,14 @@
-// Boiling point of water
+// ftoC
 package main
 
 import "fmt"
 
-const boilingF = 212.0
-
 func main() {
-	var f = boilingF
-	var c = (f - 32) * 5 / 9
-	fmt.Printf("boiling point = %gF or %gC\n", f, c)
+	var freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%gF = %gC\n", freezingF, fToC(freezingF));
+	fmt.Printf("%gF = %gC\n", boilingF, fToC(boilingF));
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
 }
