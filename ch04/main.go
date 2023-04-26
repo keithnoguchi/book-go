9eb810056f6bd777698a41ab8f9766412b928eb3
Keith Noguchi <keith@noguchi.us>
2023-04-26 06:31:31 -0700

    basic sha256 through crypto/sha256
    I love golang's standard libraries, simple and straight forward.
    
    This is one of the beauty of golang.

--- ch04/main.go
+++ ch04/main.go
@@ -1,10 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 func main() {
-	a := [2]int{1, 2}
-	b := [...]int{1, 2}
-
-	fmt.Println(a == b)
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
 }
