779e749f006086256f0300ea1ef883f93f1988bd
Keith Noguchi <keith@noguchi.us>
2023-04-26 06:06:15 -0700

    A new type with type declaration

--- ch04/main.go
+++ ch04/main.go
@@ -2,9 +2,21 @@ package main
 
 import "fmt"
 
+type Currency int
+
+const (
+	USD Currency = iota
+	EUR
+	GRP
+	RMB
+	JPY
+)
+
 func main() {
 	r := [...]int{1, 2, 3};
 	fmt.Println(len(r))
 	fmt.Println(r[2])
 	fmt.Printf("%T\n", r)
+
+	fmt.Println(JPY)
 }
