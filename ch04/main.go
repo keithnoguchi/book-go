f5edf60422804edf420414b6b27f1381a7526b35
Keith Noguchi <keith@noguchi.us>
2023-04-28 06:57:44 -0700

    ch04: appendInt function to demonstrate slice behavior in Go

--- ch04/main.go
+++ ch04/main.go
@@ -2,18 +2,31 @@ package main
 
 import "fmt"
 
-// An in-place reverse function.
-func reverse(s []int) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		z = x[:zlen]
+	} else {
+		// make zcap as twice as big than the current
+		// capacity.
+		zcap := zlen
+		// This check defends the case of len(x) == 0
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
 	}
+	z[len(x)] = y
+	return z
 }
 
 func main() {
-	a := make([]int, 20)
-	for i := range a {
-		a[i] = i
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d cap=%2d\t%v\n", i, cap(y), y)
+		x = y
 	}
-	reverse(a)
-	fmt.Println(a)
 }
