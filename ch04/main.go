204082fa8581b1adad132f98ef4c6375b838a9be
Keith Noguchi <keith@noguchi.us>
2023-04-30 08:41:45 -0700

    ch04: A stack
    Honestly, it's really cumbersome to write stack
    in Go, or I don't know the better ideom yet.

--- ch04/main.go
+++ ch04/main.go
@@ -12,6 +12,7 @@ func main() {
 	s.push(3)
 	s.push(4, 5, 6)
 	fmt.Println(s)
+	s.remove(0)
 	for {
 		if data, err := s.pop(); err == nil {
 			fmt.Println(data)
@@ -23,23 +24,32 @@ func main() {
 }
 
 type Stack struct {
-	stack []int
-}
-
-func (s *Stack) pop() (int, error) {
-	n := len(s.stack)
-	if n == 0 {
-		return 0, errors.New("empty")
-	}
-	data := s.stack[n-1]
-	s.stack = s.stack[:n-1]
-	return data, nil
-}
-
-func (s *Stack) push(data ...int) {
-	s.stack = append(s.stack, data...)
+	slice []int
 }
 
 func (s Stack) String() string {
-	return fmt.Sprintf("%v", s.stack)
+	return fmt.Sprintf("%v", s.slice)
+}
+
+func (s *Stack) push(data ...int) {
+	s.slice = append(s.slice, data...)
+}
+
+func (s *Stack) pop() (int, error) {
+	n := len(s.slice)
+	if n == 0 {
+		return 0, errors.New("empty")
+	}
+	data := s.slice[n-1]
+	s.slice = s.slice[:n-1]
+	return data, nil
+}
+
+func (s *Stack) remove(pos uint) error {
+	if int(pos) >= len(s.slice) {
+		return errors.New("out of range")
+	}
+	copy(s.slice[pos:], s.slice[pos+1:])
+	s.slice = s.slice[:len(s.slice)-1]
+	return nil
 }
