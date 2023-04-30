5aafa02ce1825dd2619e0c68d0bc017d397e1072
Keith Noguchi <keith@noguchi.us>
2023-04-30 08:07:11 -0700

    ch04: nonempty algorithm with append

--- ch04/main.go
+++ ch04/main.go
@@ -1,22 +1,20 @@
-// nonempty is an example of an in-place slice algorithm.
 package main
 
 import "fmt"
-
-func nonempty(strings []string) []string {
-	i := 0
-	for _, s := range strings {
-		if s != "" {
-			strings[i] = s
-			i++
-		}
-	}
-	return strings[:i]
-}
 
 func main() {
 	data := []string{"one", "", "three"}
 	fmt.Printf("%q\n", data)
 	fmt.Printf("%q\n", nonempty(data))
 	fmt.Printf("%q\n", data)
+}
+
+func nonempty(strings []string) []string {
+	var out []string
+	for _, s := range strings {
+		if s != "" {
+			out = append(out, s)
+		}
+	}
+	return out
 }
