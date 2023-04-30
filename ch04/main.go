c7f8ff94f774a6d81776be2c8a06ac5e9b77944c
Keith Noguchi <keith@noguchi.us>
2023-04-30 08:35:00 -0700

    ch04: A stack

--- ch04/main.go
+++ ch04/main.go
@@ -1,20 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	data := []string{"one", "", "three"}
-	fmt.Printf("%q\n", data)
-	fmt.Printf("%q\n", nonempty(data))
-	fmt.Printf("%q\n", data)
-}
-
-func nonempty(strings []string) []string {
-	var out []string
-	for _, s := range strings {
-		if s != "" {
-			out = append(out, s)
+	s := Stack{}
+	s.push(1)
+	s.push(2)
+	s.push(3)
+	s.push(4, 5, 6)
+	fmt.Println(s)
+	for {
+		if data, err := s.pop(); err == nil {
+			fmt.Println(data)
+		} else {
+			break
 		}
 	}
-	return out
+	fmt.Println(s)
+}
+
+type Stack struct {
+	stack []int
+}
+
+func (s *Stack) pop() (int, error) {
+	n := len(s.stack)
+	if n == 0 {
+		return 0, errors.New("empty")
+	}
+	data := s.stack[n-1]
+	s.stack = s.stack[:n-1]
+	return data, nil
+}
+
+func (s *Stack) push(data ...int) {
+	s.stack = append(s.stack, data...)
+}
+
+func (s Stack) String() string {
+	return fmt.Sprintf("%v", s.stack)
 }
