48ff7f62f19aee898c21b7c034d06a69b506c2fb
Keith Noguchi <keith@noguchi.us>
2023-04-30 08:00:43 -0700

    ch04: nonempty, an in-place slice algorithm example

--- ch04/main.go
+++ ch04/main.go
@@ -1,32 +1,22 @@
+// nonempty is an example of an in-place slice algorithm.
 package main
 
 import "fmt"
 
-func main() {
-	var x []int
-
-	x = appendInt(x, []int{1, 2, 3}...)
-	x = appendInt(x, 4)
-	x = appendInt(x, 5, 6, 7)
-	x = appendInt(x, x...)
-	fmt.Println(x)
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
 }
 
-func appendInt(x []int, y ...int) []int {
-	var z []int
-	zlen := len(x) + len(y)
-	if zlen <= cap(x) {
-		z = x[:zlen]
-	} else {
-		zcap := zlen
-		if zcap < 2*len(x) {
-			zcap = 2 * len(x) // at least twice as big than x
-		}
-		z = make([]int, zlen, zcap)
-		copy(z, x)
-	}
-	fmt.Println(len(z), cap(z))
-	fmt.Println(y)
-	copy(z[len(x):], y)
-	return z
+func main() {
+	data := []string{"one", "", "three"}
+	fmt.Printf("%q\n", data)
+	fmt.Printf("%q\n", nonempty(data))
+	fmt.Printf("%q\n", data)
 }
