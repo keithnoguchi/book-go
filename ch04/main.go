948c86562db0f3f9c96f2290f439f45d4b9fe1fc
Keith Noguchi <keith@noguchi.us>
2023-05-11 06:55:47 -0700

    ch04: Struct embedding
    This is soooo golang.

--- ch04/main.go
+++ ch04/main.go
@@ -1,26 +1,24 @@
-// A diercted graph
+// Struct Embedding, p. 104
 package main
 
 import "fmt"
 
-var graph map[string]map[string]float64
+type Point struct {
+	X, Y int
+}
+
+type Circle struct {
+	Center Point
+	Radius int
+}
+
+type Wheel struct {
+	Circle Circle
+	Spokes int
+}
 
 func main() {
-	graph = make(map[string]map[string]float64)
-	addEdge(graph, "A", "B")
-	fmt.Println(hasEdge(graph, "A", "B"))
-	fmt.Println(hasEdge(graph, "B", "A"))
-}
-
-func addEdge(graph map[string]map[string]float64, from, to string) {
-	// assigning to the default map value will cause panic.
-	if graph[from] == nil {
-		graph[from] = make(map[string]float64)
-	}
-	graph[from][to] = 1.1
-}
-
-func hasEdge(graph map[string]map[string]float64, from, to string) float64 {
-	// referencing the default map value won't cause panic.
-	return graph[from][to]
+	var c Circle
+	var w Wheel
+	fmt.Println(c, w)
 }
