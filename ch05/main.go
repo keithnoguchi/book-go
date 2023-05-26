5ad4de063e3288e2d27fe3bf7da78e1737fbec11
Keith Noguchi <keith@noguchi.us>
2023-05-26 12:07:13 -0700

    a reader to return the io.Reader instance

--- ch05/main.go
+++ ch05/main.go
@@ -1,35 +1,30 @@
-// A web link finder.
+// A web link lister.
 package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
-
-	"golang.org/x/net/html"
 )
 
 func main() {
-	ch := make(chan string)
+	ch := make(chan io.Reader)
 	for _, url := range os.Args[1:] {
-		go finder(url, ch)
+		go reader(url, ch)
 	}
 	for range os.Args[1:] {
-		fmt.Print(<-ch)
+		fmt.Println(<-ch)
 	}
 }
 
-func finder(url string, ch chan<- string) {
+// reader retrieves the url website and returns the io.Reader
+// over the channel.
+func reader(url string, ch chan<- io.Reader) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "http.Get() error: %s\n", err)
-		os.Exit(1)
+		fmt.Fprintf(os.Stderr, "http.Get(%s) error: %s\n", url, err)
+		return
 	}
-	defer resp.Body.Close()
-	_, err = html.Parse(resp.Body)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "html.Parse() error: %s\n", err)
-		os.Exit(1)
-	}
-	ch <- "not implemented\n"
+	ch <- resp.Body
 }
