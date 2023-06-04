5786be7d0c3218e621337947d9e2fc2da2fa69be
Keith Noguchi <keith@noguchi.us>
2023-06-04 08:12:47 -0700

    A topological sort, basically a depth first search
    This is amazing.  Let's practice a multiple times
    through out this week, week #24 of 2023.

--- ch05/main.go
+++ ch05/main.go
@@ -1,10 +1,61 @@
 // Topological Sort
+//
+// # Examples
+//
+// ```
+// go run .
+//```
 package main
 
 import (
 	"fmt"
+	"sort"
 )
 
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus": {"linear algebra"},
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organizations",
+	},
+	"data structures": {"discrete math"},
+	"databases": {"data structures"},
+	"discrete math": {"intro to programming"},
+	"formal languages": {"discrete math"},
+	"networks": {"operating systems"},
+	"operating systems": {"data structures", "computer organizations"},
+	"programming languages": {"data structures", "computer organizations"},
+}
+
 func main() {
-	fmt.Println("Topological Sort")
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i, course)
+	}
+}
+
+// A topological sort, basically, it's depth first search.
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	// let's do it!
+	sort.Strings(keys)
+	visitAll(keys)
+	return order
 }
