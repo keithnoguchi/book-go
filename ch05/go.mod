959e11316d0692c359776805468cc98e702b7808
Keith Noguchi <keith@noguchi.us>
2023-06-04 07:57:10 -0700

    ch05: A topological sort

--- ch05/go.mod
+++ ch05/go.mod
@@ -1,5 +1,3 @@
 module ch05
 
 go 1.20
-
-require golang.org/x/net v0.10.0 // indirect
--- ch05/main.go
+++ ch05/main.go
@@ -1,50 +1,10 @@
-// A html outliner
-//
-// # Examples
-//
-// ```
-// go run . https://golang.org
-//```
+// Topological Sort
 package main
 
 import (
 	"fmt"
-	"net/http"
-	"os"
-
-	"golang.org/x/net/html"
 )
 
 func main() {
-	resp, _ := http.Get(os.Args[1])
-	doc, _ := html.Parse(resp.Body)
-	forEachNode(doc, startElement, endElement)
-}
-
-var depth int
-
-func startElement(n *html.Node) {
-	if n.Type == html.ElementNode {
-		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
-		depth++
-	}
-}
-
-func endElement(n *html.Node) {
-	if n.Type == html.ElementNode {
-		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
-		depth--
-	}
-}
-
-func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
-	if pre != nil {
-		pre(n)
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		forEachNode(c, pre, post)
-	}
-	if post != nil {
-		post(n)
-	}
+	fmt.Println("Topological Sort")
 }
