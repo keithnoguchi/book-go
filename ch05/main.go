f817c32817a01aa8d2e89f6d88be19c9fa96fb15
Keith Noguchi <keith@noguchi.us>
2023-06-05 07:07:15 -0700

    ch05: A web crawler

--- ch05/main.go
+++ ch05/main.go
@@ -1,61 +1,86 @@
-// Topological Sort
+// A web crawler
 //
-// # Examples
+// # Example
 //
-// ```
-// go run .
-// ```
+// 111
+// go run . https://golang.org
+//```
 package main
 
 import (
 	"fmt"
-	"sort"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
 )
 
-var prereqs = map[string][]string{
-	"algorithms": {"data structures"},
-	"calculus":   {"linear algebra"},
-	"compilers": {
-		"data structures",
-		"formal languages",
-		"computer organizations",
-	},
-	"data structures":       {"discrete math"},
-	"databases":             {"data structures"},
-	"discrete math":         {"intro to programming"},
-	"formal languages":      {"discrete math"},
-	"networks":              {"operating systems"},
-	"operating systems":     {"data structures", "computer organizations"},
-	"programming languages": {"data structures", "computer organizations"},
+func main() {
+	breathFirst(crawl, os.Args[1:])
 }
 
-func main() {
-	for i, course := range topoSort(prereqs) {
-		fmt.Printf("%d:\t%s\n", i+1, course)
+func crawl(url string) []string {
+	fmt.Println(url)
+	links, err := extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return links
+}
+
+func breathFirst(f func(string) []string, work []string) {
+	seen := make(map[string]bool)
+	for len(work) > 0 {
+		items := work
+		work = nil
+		for _, item := range items {
+			if seen[item] {
+				continue
+			}
+			seen[item] = true
+			work = append(work, f(item)...)
+		}
 	}
 }
 
-// A topological sort, basically, it's depth first search.
-func topoSort(m map[string][]string) []string {
-	var order []string
-	seen := make(map[string]bool)
-	var visitAll func(items []string)
-	visitAll = func(items []string) {
-		for _, item := range items {
-			if !seen[item] {
-				seen[item] = true
-				visitAll(m[item])
-				order = append(order, item)
+func extract(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %v", url, err)
+	}
+	var links []string
+	visit := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue // ignore bad URLs
+				}
+				links = append(links, link.String())
 			}
 		}
 	}
-	var keys []string
-	for key := range m {
-		keys = append(keys, key)
-	}
+	forEach(doc, visit, nil)
+	return links, nil
+}
 
-	// let's do it!
-	sort.Strings(keys)
-	visitAll(keys)
-	return order
+func forEach(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEach(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
 }
