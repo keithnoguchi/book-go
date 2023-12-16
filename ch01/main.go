c0081764cf781e6c8ac7a173cf3722047e1b34f8
Keith Noguchi <keith@noguchi.us>
2023-12-16 07:57:42 -0800

    ch01: Lissajous figures, once again
    I start to love Golang, again. It's soooo simple. :)

--- ch01/main.go
+++ ch01/main.go
@@ -1,80 +1,22 @@
-// A lissajous web server
-//
-// # Examples
-//
-// ```
-// $ go run main.go
-// ```
+// SPDX-License-Identifier: GPL-2.0
 package main
 
 import (
-	"context"
-	"image"
 	"image/color"
-	"image/gif"
-	"log"
-	"math"
-	"math/rand"
-	"net/http"
-	"sync"
-	"time"
+	"io"
+	"os"
 )
 
 var palette = []color.Color{color.White, color.Black}
 
 const (
-	whiteIndex = 0
+	whilteIndex = 0
 	blackIndex = 1
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	s := &http.Server{Addr: "localhost:8000"}
-
-	// Fires up the server goroutine.
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		if err := s.ListenAndServe(); err != http.ErrServerClosed {
-			log.Fatal(err)
-		}
-	}()
-
-	// Shutdowns the server after five seconds
-	time.Sleep(5 * time.Second)
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Fatal(err)
-	}
-	wg.Wait()
+	lissajous(os.Stdout)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	const (
-		cycles  = 5
-		res     = 0.001
-		size    = 100
-		nframes = 64
-		delay   = 8
-	)
-	freq := rand.Float64() * 3.0
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(
-				size+int(x*size+0.5),
-				size+int(y*size+0.5),
-				blackIndex,
-			)
-		}
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
-	}
-	gif.EncodeAll(w, &anim)
+func lissajous(out io.Writer) {
 }
