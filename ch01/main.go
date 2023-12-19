80f38db1e6f2913533017647dfad65221930dc4e
Keith Noguchi <keith@noguchi.us>
2023-12-19 07:26:28 -0800

    ch01: A simple web server

--- ch01/main.go
+++ ch01/main.go
@@ -1,38 +1,17 @@
-// fetch fetches the html body concurrently.
+// A simple web server.
 package main
 
 import (
 	"fmt"
-	"io"
+	"log"
 	"net/http"
-	"os"
-	"time"
 )
 
-func main() {
-	start := time.Now()
-	ch := make(chan string)
-	for _, url := range os.Args[1:] {
-		go fetch(url, ch)
-	}
-	for range os.Args[1:] {
-		fmt.Println(<-ch)
-	}
-	fmt.Printf("%.2fs elasped\n", time.Since(start).Seconds())
+func main()  {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
-func fetch(url string, ch chan<- string) {
-	start := time.Now()
-	resp, err := http.Get(url)
-	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
-	}
-	nbytes, err := io.Copy(io.Discard, resp.Body)
-	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
-	}
-	sec := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs %7d %s", sec, nbytes, url)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
