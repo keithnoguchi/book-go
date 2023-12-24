34cf55590cacf17b99015706d8f597fd111128d1
Keith Noguchi <keith@noguchi.us>
2023-12-24 12:02:08 -0800

    ch01: A Lissajous web server

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Examples from [the Go Programming Language], aka, the book.
 
 ## Examples
 
-- [Chapter 1: A web server](ch01/main.go)
+- [Chapter 1: A Lissajous web server](ch01/main.go)
 - [Chapter 2: A pop counter](ch02/main.go)
 - [Chapter 3: A mandelbrot web server](ch03/main.go)
 - [Chapter 5: A web link finder](ch05/main.go)
--- ch01/main.go
+++ ch01/main.go
@@ -1,28 +1,60 @@
-// A web server
+// A Lissajous web server
 package main
 
 import (
 	"fmt"
-	"log"
+	"image"
+	"image/color"
+	"image/gif"
+	"math"
+	"math/rand"
 	"net/http"
+	"log"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", lissajous)
+	http.HandleFunc("/hello", hello)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!\n")
+}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+func lissajous(w http.ResponseWriter, r *http.Request) {
+	const (
+		cycles = 10
+		res = 0.001
+		size = 200
+		nframes = 64
+		delay = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(
+				size+int(x*size+0.5),
+				size+int(y*size+0.5),
+				blackIndex,
+			)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
 	}
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
-	}
-	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
-	}
+	gif.EncodeAll(w, &anim)
 }
