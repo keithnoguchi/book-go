588739da38eded79ae34061f0f149b01c008540c
Keith Noguchi <keith@noguchi.us>
2024-01-17 22:32:55 -0800

    stack with slice

--- ch04/main.go
+++ ch04/main.go
@@ -4,18 +4,25 @@ package main
 import "fmt"
 
 func main() {
-	data := []string{"one", "", "three"}
-	fmt.Printf("%q\n", data)
-	fmt.Printf("%q\n", nonempty(data))
-	fmt.Printf("%q\n", data)
+	stack := []int{}
+	stack = push(stack, 1)
+	stack = push(stack, 99, 2020)
+	fmt.Printf("%v\n", stack)
+	for range stack {
+		var x int
+		stack, x = pop(stack)
+		fmt.Printf("%v\n", x)
+		fmt.Printf("%v\n", stack)
+	}
+	//stack, x := pop(stack) panic...
+	//fmt.Printf("%v\n", x)
 }
 
-func nonempty(data []string) []string {
-	out := data[:0]
-	for _, s := range data {
-		if s != "" {
-			out = append(out, s)
-		}
-	}
-	return out
+func push(stack []int, data ...int) []int {
+	return append(stack, data...)
+}
+
+func pop(stack []int) ([]int, int) {
+	x := stack[len(stack)-1]
+	return stack[:len(stack)-1], x
 }
