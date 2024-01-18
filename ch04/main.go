1db81f211fb6574d56800425c5a6041c7369c18a
Keith Noguchi <keith@noguchi.us>
2024-01-17 17:36:33 -0800

    ch04: reverse(a []int)

---
+++ ch04/main.go
@@ -0,0 +1,17 @@
+// Reverse function
+package main
+
+import "fmt"
+
+func main() {
+	a := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(a)
+	reverse(a[:])
+	fmt.Println(a)
+}
+
+func reverse(a []int) {
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+}
