ec76b4409a0d404c8df24a7d27a68604773e3ef7
Keith Noguchi <keith@noguchi.us>
2024-01-17 17:46:32 -0800

    appendInt() function

--- ch04/main.go
+++ ch04/main.go
@@ -4,14 +4,27 @@ package main
 import "fmt"
 
 func main() {
-	a := [...]int{1, 2, 3, 4, 5}
-	fmt.Println(a)
-	reverse(a[:])
-	fmt.Println(a)
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d  cap=%d\t%v\n", i, cap(y), y)
+		x = y
+	}
 }
 
-func reverse(a []int) {
-	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
-		a[i], a[j] = a[j], a[i]
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		z = x[:zlen]
+	} else {
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
 	}
+	z[zlen-1] = y
+	return z
 }
