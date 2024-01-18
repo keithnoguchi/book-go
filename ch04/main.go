d3ebc8ead52e0b00d053aceec7c4545a76af56ff
Keith Noguchi <keith@noguchi.us>
2024-01-17 17:40:58 -0800

    revert reverse() back to the slice argument

--- ch04/main.go
+++ ch04/main.go
@@ -6,11 +6,11 @@ import "fmt"
 func main() {
 	a := [...]int{1, 2, 3, 4, 5}
 	fmt.Println(a)
-	reverse(&a)
+	reverse(a[:])
 	fmt.Println(a)
 }
 
-func reverse(a *[5]int) {
+func reverse(a []int) {
 	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
 		a[i], a[j] = a[j], a[i]
 	}
