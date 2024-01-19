eccc435385970c7b1dad95359c1fa2a7384dfae2
Keith Noguchi <keith@noguchi.us>
2024-01-19 12:05:10 -0800

    ch04: UTF8 aware char counter

--- ch04/main.go
+++ ch04/main.go
@@ -1,24 +1,48 @@
-// `dedup`
+// UTF8 aware charcounter
 package main
 
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"unicode"
+	"unicode/utf8"
 )
 
 func main() {
-	seen := make(map[string]bool)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		line := input.Text()
-		if !seen[line] {
-			seen[line] = true
-			fmt.Println(line)
+	counts := make(map[rune]int)
+	var utflens [utf8.UTFMax + 1]int
+	invalid := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflens[n]++
+	}
+	fmt.Printf("rune\tcount\n")
+	for r, n := range counts {
+		fmt.Printf("%-5q %3d\n", r, n)
+	}
+	fmt.Printf("\nlen\tcount\n")
+	for i, n := range utflens {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
 		}
 	}
-	if err := input.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "dedup: %v\n", err)
-		os.Exit(1)
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
 	}
 }
