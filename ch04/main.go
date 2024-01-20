36708d3bb359b4571129e85e7c3710f343074329
Keith Noguchi <keith@noguchi.us>
2024-01-20 09:40:43 -0800

    ch04: An insertion sort with tree

--- ch04/main.go
+++ ch04/main.go
@@ -1,32 +1,56 @@
-// Graph
+// An insertion sort
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
-type graph map[string]map[string]bool
+type tree struct {
+	value       int
+	left, right *tree
+}
 
 func main() {
-	g := newGraph()
-	g.addEdge("a", "b")
-	g.addEdge("b", "a")
-	g.addEdge("a", "c")
-	fmt.Println(g)
-	fmt.Println(g.hasEdge("a", "b"))
-	fmt.Println(g.hasEdge("a", "c"))
-	fmt.Println(g.hasEdge("c", "a"))
-}
-
-func newGraph() graph {
-	return make(map[string]map[string]bool)
-}
-
-func (g graph) addEdge(from, to string) {
-	if g[from] == nil {
-		g[from] = make(map[string]bool)
+	// random numbers
+	values := make([]int, 0, 100)
+	for i := 0; i < 100; i++ {
+		values = append(values, rand.Int()%100)
 	}
-	g[from][to] = true
+	fmt.Printf("%v...\n", values[:16])
+	sort(values)
+	fmt.Printf("%v ... %v\n", values[0:8], values[len(values)-8:])
 }
 
-func (g graph) hasEdge(from, to string) bool {
-	return g[from][to]
+func sort(values []int) []int {
+	var t *tree
+	for _, v := range values {
+		t = add(t, v)
+	}
+	appendValue(values[:0], t)
+	return values
+}
+
+func appendValue(values []int, t *tree) []int {
+	if t == nil {
+		return values
+	}
+	values = appendValue(values, t.left)
+	values = append(values, t.value)
+	values = appendValue(values, t.right)
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
 }
