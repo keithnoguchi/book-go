3900b8d1555fe912a12626ffd90a76e5e0844f18
Keith Noguchi <keith@noguchi.us>
2024-02-05 07:41:05 -0800

    ch04: Composite Types (#9)
    A GitHub issue search.

---
+++ ch04/main.go
@@ -0,0 +1,48 @@
+// A github issue searcher
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+const issuesURL = "https://api.github.com/search/issues"
+
+func main() {
+	q := url.QueryEscape(strings.Join(os.Args[1:], " "))
+	resp, err := http.Get(issuesURL + "?q=" + q)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("HTTP status code: %d", resp.StatusCode)
+	}
+	var issues Issues
+	if err := json.NewDecoder(resp.Body).Decode(&issues); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", issues.TotalCount)
+	for _, item := range issues.Items {
+		fmt.Printf(
+			"#%-5d %9.9s %.55s\n",
+			item.Number, item.Title, item.Body,
+		)
+	}
+}
+
+type Issues struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
+type Issue struct {
+	Number int
+	Title  string
+	Body   string
+}
