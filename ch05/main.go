5b1135de71c3cbfc0f5d420578361c4eff7caafc
Keith Noguchi <keith@noguchi.us>
2024-02-05 07:56:02 -0800

    A http getter as a start

--- ch05/main.go
+++ ch05/main.go
@@ -1,8 +1,10 @@
-// A web link finder
+// A web link visitor
 package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"sync"
 )
@@ -18,5 +20,15 @@ func main() {
 
 func visitor(wg *sync.WaitGroup, url string) {
 	defer wg.Done()
-	fmt.Println(url)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "visitor: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	n, err := io.Copy(io.Discard, resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "visitor: %v\n", err)
+	}
+	fmt.Println(url, n)
 }
