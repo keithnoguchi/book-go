72cc9fcd00019c8da753b7a0d7532e50c0f34c03
Keith Noguchi <keith@noguchi.us>
2024-02-13 07:05:33 -0800

    5.5 Function Values!

--- ch05/main.go
+++ ch05/main.go
@@ -1,53 +1,46 @@
-// A web link visitor
+// 5.5 Function Values
 package main
 
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"golang.org/x/net/html"
 )
 
 func main() {
-	result := make(chan []string)
-	for _, url := range os.Args[1:] {
-		go visiter(result, url)
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
 	}
-	for range os.Args[1:] {
-		links := <- result
-		for i, link := range links {
-			fmt.Println(i, link)
-		}
+	forEachNode(doc, startElement, endElement)
+}
+
+var depth int
+
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		depth++
 	}
 }
 
-func visiter(out chan<- []string, url string) {
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Printf("get(%q): %v", url, err)
-		out <- nil
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		depth--
 	}
-	defer resp.Body.Close()
-	doc, err := html.Parse(resp.Body)
-	if err != nil {
-		log.Printf("parse(%q): %v", url, err)
-		out <- nil
-	}
-	out <- visit(nil, doc)
 }
 
-func visit(link []string, n *html.Node) []string {
-	if n.Type == html.ElementNode && n.Data == "a" {
-		for _, a := range n.Attr {
-			if a.Key == "href" {
-				link = append(link, a.Val)
-			}
-		}
+func forEachNode(n *html.Node, pre, post func(*html.Node)) {
+	if pre != nil {
+		pre(n)
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		link = visit(link, c)
+		forEachNode(c, pre, post)
 	}
-	return link
+	if post != nil {
+		post(n)
+	}
 }
