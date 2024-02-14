009600ee043a5b151dc058c97690c5ee9517601a
Keith Noguchi <keith@noguchi.us>
2024-02-14 06:52:24 -0800

    What a wonderful anonymous functions, aka closures
    A Code with State, Rust async fn is the same
    as the anonymous functions in Go.

--- ch05/main.go
+++ ch05/main.go
@@ -5,4 +5,17 @@ import "fmt"
 
 func main() {
 	fmt.Println("5.6 Anonymous Functions")
+	f := squares()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+}
+
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
 }
